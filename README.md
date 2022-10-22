7050d1327fdba88cdbaed7077b761fbc6f398920
Rick van de Loo <rickvandeloo@gmail.com>
2022-10-22 20:34:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hypernode API Python Client
+
+_**Please note: this project is still in its early stages and the API may be subject to change.**_
+
+## Installation
+
+```bash
+git clone https://github.com/byteinternet/hypernode-api-python.git
+cd hypernode-api-python
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements/development.txt
+```
+
+## Usage
+
+### Acquiring an API token
+
+Each Hypernode has an API token associated with it, you can use that to talk to the API directly. You can find the token in `/etc/hypernode/hypernode_api_token`. For API tokens with special permissions please contact support@hypernode.com.
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+from os.path import abspath, dirname, join
+
+
+def readfile(filename):
+    path = join(dirname(abspath(__file__)), filename)
+    with open(path, 'rt') as filehandle:
+        return filehandle.read()
+
+
+setup(
+    name='hypernode_api_python',
+    version='0.0.1',
+    description='"Hypernode API Client for Python"',
+    long_description=readfile('README.rst'),
+    long_description_content_type='text/x-rst',
+    author='Hypernode',
+    author_email='support@hypernode.com',
+    license='MIT',
+    url='https://github.com/ByteInternet/hypernode_api_python',
+    packages=['hypernode_api_python'],
+    install_requires=['pip'],
+    entry_points={
+        'console_scripts': []
+    }
+)
