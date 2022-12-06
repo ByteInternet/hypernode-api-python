a17b9eedd28576d738bce675d884048c10b8ba20
Rick van de Loo <rickvandeloo@gmail.com>
2022-12-06 17:46:27 +0100

    add setup.py for PyPI
    so we can upload to PyPI

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
-from setuptools import setup
-from os.path import abspath, dirname, join
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
-def readfile(filename):
-    path = join(dirname(abspath(__file__)), filename)
-    with open(path, "rt") as filehandle:
-        return filehandle.read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+requirements = """
+pip
+requests
+"""
 
 
 setup(
     name="hypernode_api_python",
     version="0.0.1",
     description='"Hypernode API Client for Python"',
-    long_description=readfile("README.md"),
-    long_description_content_type="text/markdown",
-    author="Hypernode",
-    author_email="support@hypernode.com",
-    license="MIT",
     url="https://github.com/ByteInternet/hypernode_api_python",
-    packages=["hypernode_api_python"],
-    install_requires=["pip"],
-    entry_points={"console_scripts": []},
+    packages=find_packages(
+        include=["hypernode_api_python", "requirements/base.txt"], exclude=["tests"]
+    ),
+    author="Hypernode Team",
+    author_email="support@hypernode.com",
+    install_requires=requirements.split("\n"),
+    python_requires=">=3.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
 )
