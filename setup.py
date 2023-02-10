8a30fd7cbb50d0a0541359033e27cab5123d0c0b
alejandro <alejandrogarz@gmail.com>
2023-02-10 11:26:41 +0100

    feat: update version number in package and readme

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requests
 
 setup(
     name="hypernode_api_python",
-    version="0.0.3",
+    version="0.0.4",
     description='"Hypernode API Client for Python"',
     url="https://github.com/ByteInternet/hypernode_api_python",
     packages=find_packages(
